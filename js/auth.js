1fc717018067e0b17ab6c8054b8eed724e83af81
Mohammad khan <khankadir2703@gmail.com>
2026-01-29 22:50:48 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,20 @@
+const user = localStorage.getItem("pf_user");
+
+function login(){
+const name=document.getElementById("username").value;
+if(!name) return alert("Enter name");
+localStorage.setItem("pf_user",name);
+location.reload();
+}
+
+function logout(){
+localStorage.removeItem("pf_user");
+location.href="index.html";
+}
+
+if(user){
+document.getElementById("loginBox")?.remove();
+document.getElementById("logoutBtn")?.classList.remove("hidden");
+document.getElementById("name") && (document.getElementById("name").innerText=user);
+}
 
