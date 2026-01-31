5d3b520b812be20f54e6b9db165f5b2e0001068f
Mohammad khan <khankadir2703@gmail.com>
2026-02-01 00:22:24 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
-// firebase-config.js
+// Aapke naye project ka configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjBHx3xSsfswnaY20PpR8T4YTe9ckFr6E", // यह आपका लेटेस्ट API Key है
-  authDomain: "playforge-dda06.firebaseapp.com",
-  projectId: "playforge-dda06",
-  storageBucket: "playforge-dda06.firebasestorage.app",
-  messagingSenderId: "540319303458",
-  appId: "1:540319303458:web:47b4e09d33282fb658077b"
+  apiKey: "AIzaSyC_ed7CPLdUtM2vFWJLjT7r-7n5iw1XlVU",
+  authDomain: "playforge-d46a3.firebaseapp.com",
+  projectId: "playforge-d46a3",
+  storageBucket: "playforge-d46a3.firebasestorage.app",
+  messagingSenderId: "347695622009",
+  appId: "1:347695622009:web:f8fe89ddc24363ab4571a8",
+  measurementId: "G-KRXZE9XC5S"
 };
 
-// Initialize Firebase (आसान तरीका)
+// Firebase initialize karein
 firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 const db = firebase.firestore();
-
-console.log("Firebase Connected!");
